691752bd6cdcb9c588802c91120974a18cc2d724
Philipp Dahlinger <philipp.dahlinger@kit.edu>
2024-02-09 15:58:26 +0100

    initial commit

---
+++ setup.py
@@ -0,0 +1,39 @@
+# Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
+# type: ignore
+from setuptools import find_namespace_packages, setup
+
+with open("README.md") as fh:
+    LONG_DESC = fh.read()
+    setup(
+        name="hydra-list-sweeper",
+        version="1.0.0",
+        author="Philipp Dahlinger",
+        author_email="philipp.dahlinger@kit.edu",
+        description="List Hydra Sweeper plugin",
+        long_description=LONG_DESC,
+        long_description_content_type="text/markdown",
+        url="https://github.com/facebookresearch/hydra/",
+        packages=find_namespace_packages(include=["hydra_plugins.*"]),
+        classifiers=[
+            # Feel free to use another license.
+            "License :: OSI Approved :: MIT License",
+            # Hydra uses Python version and Operating system to determine
+            # In which environments to test this plugin
+            "Programming Language :: Python :: 3.7",
+            "Programming Language :: Python :: 3.8",
+            "Programming Language :: Python :: 3.9",
+            "Programming Language :: Python :: 3.10",
+            "Programming Language :: Python :: 3.11",
+            "Operating System :: OS Independent",
+        ],
+        install_requires=[
+            # consider pinning to a specific major version of Hydra to avoid unexpected problems
+            # if a new major version of Hydra introduces breaking changes for plugins.
+            # e.g: "hydra-core==1.0.*",
+            "hydra-core",
+        ],
+        # If this plugin is providing configuration files, be sure to include them in the package.
+        # See MANIFEST.in.
+        # For configurations to be discoverable at runtime, they should also be added to the search path.
+        include_package_data=True,
+    )
---
+++ tests/test_example_sweeper_plugin.py
@@ -0,0 +1,96 @@
+# Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
+from pytest import mark
+
+from hydra.core.plugins import Plugins
+from hydra.plugins.sweeper import Sweeper
+from hydra.test_utils.launcher_common_tests import (
+    BatchedSweeperTestSuite,
+    IntegrationTestSuite,
+    LauncherTestSuite,
+)
+from hydra.test_utils.test_utils import TSweepRunner
+
+from hydra_plugins.example_sweeper_plugin.example_sweeper import ExampleSweeper
+
+
+def test_discovery() -> None:
+    # Tests that this plugin can be discovered via the plugins subsystem when looking at the Sweeper plugins
+    assert ExampleSweeper.__name__ in [
+        x.__name__ for x in Plugins.instance().discover(Sweeper)
+    ]
+
+
+def test_launched_jobs(hydra_sweep_runner: TSweepRunner) -> None:
+    sweep = hydra_sweep_runner(
+        calling_file=None,
+        calling_module="hydra.test_utils.a_module",
+        config_path="configs",
+        config_name="compose.yaml",
+        task_function=None,
+        overrides=["hydra/sweeper=example", "hydra/launcher=basic", "foo=1,2"],
+    )
+    with sweep:
+        assert sweep.returns is not None
+        job_ret = sweep.returns[0]
+        assert len(job_ret) == 2
+        assert job_ret[0].overrides == ["foo=1"]
+        assert job_ret[0].cfg == {"foo": 1, "bar": 100}
+        assert job_ret[1].overrides == ["foo=2"]
+        assert job_ret[1].cfg == {"foo": 2, "bar": 100}
+
+
+# Run launcher test suite with the basic launcher and this sweeper
+@mark.parametrize(
+    "launcher_name, overrides",
+    [
+        (
+            "basic",
+            [
+                # CHANGE THIS TO YOUR SWEEPER CONFIG NAME
+                "hydra/sweeper=example"
+            ],
+        )
+    ],
+)
+class TestExampleSweeper(LauncherTestSuite):
+    ...
+
+
+# Many sweepers are batching jobs in groups.
+# This test suite verifies that the spawned jobs are not overstepping the directories of one another.
+@mark.parametrize(
+    "launcher_name, overrides",
+    [
+        (
+            "basic",
+            [
+                # CHANGE THIS TO YOUR SWEEPER CONFIG NAME
+                "hydra/sweeper=example",
+                # This will cause the sweeper to split batches to at most 2 jobs each, which is what
+                # the tests in BatchedSweeperTestSuite are expecting.
+                "hydra.sweeper.max_batch_size=2",
+            ],
+        )
+    ],
+)
+class TestExampleSweeperWithBatching(BatchedSweeperTestSuite):
+    ...
+
+
+# Run integration test suite with the basic launcher and this sweeper
+@mark.parametrize(
+    "task_launcher_cfg, extra_flags",
+    [
+        (
+            {},
+            [
+                "-m",
+                # CHANGE THIS TO YOUR SWEEPER CONFIG NAME
+                "hydra/sweeper=example",
+                "hydra/launcher=basic",
+            ],
+        )
+    ],
+)
+class TestExampleSweeperIntegration(IntegrationTestSuite):
+    pass
